4f6fd931269ec62f5e2b7ef4e99e9916f2950801
tjaszerovnik <tjas.zerovnik.18051@dijak.sc-celje.si>
2024-11-15 18:14:19 +0100

    Ločitev v bloke in dodani presledki za vrsticami kode
    dodana metoda za ločevanje vrstic kode v bloke, dodana metoda za dodajanje presledkov na koncu vrstic kode, ki je narejena zaradi napak pri ločevanju vrstic v bloke, če je vrstica kode v večih vrstah

--- CChing/Main Program/Main Program.cpp
+++ CChing/Main Program/Main Program.cpp
@@ -20,13 +20,26 @@ void pridobiTekst(string& s, const string& potDoDatoteke)
     }
     else {
         while (getline(f, tempS)) {
-            s += tempS;
+            s += tempS + "\n";
         }
         f.close();
     }
 }
+// Za vsako vrstico doda presledek, zaradi napake pri ločitvi v blokih
+void DodajPresledekZaVrsticami(string& koda)
+{
+    stringstream ss(koda);
+    string token;
+    string result;
 
-//loči vse vrstice kode po znaku podpičja
+    while (getline(ss, token, '\n'))
+    {
+        result += token + " ";
+    }
+
+    koda = result;
+}
+//loči vse vrstice kode po znaku ;
 list<string> lociPoVejicah(const string& s)
 {
     list<string> vrsticeKode;
@@ -34,18 +47,61 @@ list<string> lociPoVejicah(const string& s)
     string token;
 
 
-    while (getline(ss, token, ';')) {
+    while (getline(ss, token, ';')) 
+    {
         vrsticeKode.push_back(token); 
     }
 
     return vrsticeKode;
 }
 
+// vsako vrstico kode razčleni na posamezne dele
+// primer: '"cwrite = "Hello World!"' razstavi na 'cwrite','=','"Hello','World!"' 
+list<list<string>> lociVrsticePoBlockih(const list<string>& vrstice) 
+{
+    list<list<string>> list2D; 
+
+    for (const string& vrstica : vrstice) 
+    {
+        list<string> list1D;  
+        stringstream ss(vrstica);
+        string token;
+
+  
+        while (ss >> token) 
+        {
+            list1D.push_back(token);
+        }
+
+        list2D.push_back(list1D);
+    }
+    return list2D;
+    
+    /*for (const auto& row : list2D)
+    {
+        for (const auto& elem : row) 
+        {
+            cout << elem << endl;
+        }
+        cout << "---" << endl;
+    }*/
+}
 int main()
 {
     string koda;
     pridobiTekst(koda, "Test1.txt");
+    DodajPresledekZaVrsticami(koda);
     list<string> tokens = lociPoVejicah(koda);
+    list<list<string>> razclenjenaKoda = lociVrsticePoBlockih(tokens);
+
+    for (const auto& row : razclenjenaKoda)
+    {
+        for (const auto& elem : row)
+        {
+            cout << elem << endl;
+        }
+        cout << "---" << endl;
+    }
 
 
     return 0;
